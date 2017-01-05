3eaa93330e478788d070030e859c641a3767793f
Alpaca-kun <ra90558@uem.br>
2017-01-05 03:09:34 -0200

    Edicao do note

--- Prototipo.cpp
+++ Prototipo.cpp
@@ -1,17 +1,58 @@
+/*
+ * Open List -> armazenar ordenadamente as possibilidades abertas de um nó, podendo abrir no máximo por 4 nó
+ * Closed List -> armazenar todos os nós já visitados
+ * Map -> concatenação das listas, para verificar a existência de uma possibilidade
+ * 
+ */
+
 #include <iostream>
-#include <tr1/unordered_map>
-#include <tr1/string>
-#include <tr1/queue>
+#include <unordered_map>
+#include <string>
+#include <queue>
 
 #define solution "00010203040506070809101112131415";
 
-using namespace std;
-using namespace tr1;
+using namespace std::tr1;
 //using namespace *** cost more
 
-typedef unordered_map <string, int> setOfLists;
-typedef priority_queue <int> openList;
+typedef struct 
 
+typedef unordered_map <string, int> unoMap;
+typedef priority_queue <int> prioQue;
+
+int verifyError (int actualTable[]) {
+  int error = 0;
+
+  for (int i = 0; i < 15; i++)
+    if (actualTable[i] != i + 1)
+      error++;
+
+  if (actualTable[i] != 0)
+    error++;
+
+  return error;
+}
+
+string intToString () {
+  string s;
+
+  for (int i = 0; i < 16; i++) {
+    cin >> initialTable[i];
+    if (initialTable[i] < 10)
+      s += "0";
+    s += to_string(initialTable[i]);
+  }
+
+  return s;
+}
+
+
+int heuristicOne () {
+  int initialTable[16];
+  int totalCost = 0,
+      height = 0,
+      heuristic = 0;
+}
 
 int main () {
   int initialTable[16];
@@ -24,7 +65,5 @@ int main () {
       s += "0";
     s += to_string(initialTable[i]);
   }
-
-  
 
 }
